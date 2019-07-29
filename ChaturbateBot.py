c97c4831b79bb53d303f5bed535d11dbe9fd4304
fuomag9 <fuomag@gmail.com>
2019-07-30 00:26:40 +0200

    New exceptions implementation in view_stream_image_callback

--- ChaturbateBot.py
+++ ChaturbateBot.py
@@ -372,6 +372,18 @@ def view_stream_image_callback(update, CallbackContext):
     except Exceptions.ModelPassword:
         send_message(chatid, f"The model {username} cannot be seen because is password protected", bot)
         logging.warning(f'{chatid} could not view {username} image update because is password protected')
+    except Exceptions.ModelDeleted:
+        send_message(chatid, f"The model {username} cannot be seen because is deleted", bot)
+        logging.warning(f'{chatid} could not view {username} image update because is deleted')
+    except Exceptions.ModelBanned:
+        send_message(chatid, f"The model {username} cannot be seen because is banned", bot)
+        logging.warning(f'{chatid} could not view {username} image update because is banned')
+    except Exceptions.ModelGeoblocked:
+        send_message(chatid, f"The model {username} cannot be seen because is geoblocked", bot)
+        logging.warning(f'{chatid} could not view {username} image update because is geoblocked')
+    except Exceptions.ModelCanceled:
+        send_message(chatid, f"The model {username} cannot be seen because is canceled", bot)
+        logging.warning(f'{chatid} could not view {username} image update because is canceled')
     except Exceptions.ModelNotViewable:
         send_message(chatid, f"The model {username} probably does not exist", bot)
         logging.warning(f'{chatid} could not view {username} image update')
